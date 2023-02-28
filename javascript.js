bbdf3a257621ab6c39856ed7dde0e649c9a6412f
Valentina P <106148603+valentina312@users.noreply.github.com>
2023-02-28 10:59:33 +0100

    Add pseudocode

---
+++ javascript.js
@@ -0,0 +1,22 @@
+/* Simulate the computer random
+choice of rock paper or scissors
+*/
+
+function getComputerChoice () {
+
+}
+
+/* Function  called playRound
+that simulate a round
+it takes as parameters playerSelection
+and computerSelection and return a string
+that declares the winner */
+
+
+/* Function called game that calls
+playRound to play 5 games with a for loop.
+Every loop should prompt the user,
+show the result with console log
+and keep the score */
+
+
